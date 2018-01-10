e6c98749b1ae5e9a0e2a9bee41dfba577006a1cb
Benjamin Tiernan <btiernan@STINS.int>
2018-01-10 10:13:04 -0500

    Added lines. Please note that this entire project is still a work in progress.

--- include/gener_comm.h
+++ include/gener_comm.h
@@ -4,6 +4,8 @@
 #ifndef general_comments
 #define general_comments
 
+const static char head_str = '[';
+const static char head_trm = ']';
 const static char splitter = ':';
 const static char sequence = ',';
 
@@ -39,5 +41,45 @@ record_value string_to_val( char *record_contents, int *record_length )
     char *lang_name
 }
 
+
+int quick_comp(char *f1, char *f2)
+{
+    // unfortunately, this looks like this will be a slow algorithm, but we'll have to see.
+    int a,b,c = 0,0,1; // I'm not overly confident that this will work in C.
+    a = strlen(f1);
+    b = strlen(f2);
+    if (a==b)
+    {
+        int d = b;
+        while(b>=0)
+        {
+            if(f1+b!=f2+b)
+            {
+                break;
+            }
+            b--;
+            if(b<0){ c=0; }
+        }
+    }
+    return c; // will return 1 if it is not equal - otherwise, will return 0.
+}
+
+// skip to the index within a file that contains the designated header.
+// this is going to be a VERY rough function to try and speed through the INI files. 
+int skip_to_section(FILE file_name, char* ini_header)
+{
+    int i = 0; // initialize the beginning index of the file.
+    int j = 0; // last index - use this as a jumping off point.
+    int k = 0; // final index - use this to remember where we left off.
+    // maybe use strncmp to make sure that the string matches? or should I actually care?
+    FILE *working_file = fopen(file_name,'r');
+    char *cmpchr;
+    fgets(cmpchr,0,1,working_file);
+    while(cmpch!='\0')
+    {
+        // if the string matches the designated title - then i'll break. I'm not sure what the rules for strncmp are....
+        
+    }
+}
 
 #endif
